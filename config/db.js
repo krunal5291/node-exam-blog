4201209ef88e8b6ca0c0dddaaca11fab9d5c485c
krunal5291 <krunalkalathiya11@gmail.com>
2024-08-20 09:54:17 +0530

    link

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 require("dotenv").config()
 const connection = ()=>{
-    mongoose.connect(process.env.DB_URL)
+    mongoose.connect("mongodb://localhost:27017")
     console.log("db connected");
 }
 
